23378c0bfef1c1d539fa1a4f274f9d76b41e0b13
thedraxx <thedraxxgames@gmail.com>
2024-10-14 15:18:23 -0300

    fin seccion 11

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //I18nSelectPipe
+  public name: string = "Fernando";
+  public gender: 'male' | 'famale' = "male";
+  public invitationMap = { male: "invitarlo", famale: "invitarla" }
+
+  public changeClient(): void {
+    this.name = "melisa"
+    this.gender = "famale"
+  }
+
+  // I18nPluralPipe
+  public clients: string[] = ["maria", "fernando", "Hernan", "Pedro"]
+  public clientsMap = {
+    "=0": "ningun cliente esperando",
+    "other": "# clientes esperando",
+  }
+
+  public deleteClient(): void {
+    this.clients.shift();
+  }
+
+
+  // KeyValuePipe
+  public person = {
+    name: "fernando",
+    age: 23,
+    adress: "okinawa"
+  }
+
+  //AsyncPipe
+  public myObservableTimer = interval(2000).pipe(tap(value => {
+    console.log('tap', value)
+  }))
+
+  public promiseValue = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("tenemos data en la promesa");
+      console.log("tenemos data en la primersa ")
+      this.person.name = "otro nombre"
+    }, 5000);
+  })
 }
